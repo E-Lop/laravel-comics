f82541de5885bc506aa0386c9564b6a475c868fb
E-Lop <edmondolopez@gmail.com>
2022-08-24 15:58:25 +0200

    scheletro header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,51 @@
-<h1>ciao sono l'header</h1>
+<header>
+    <div>
+        <div class="container">
+          <section>
+            <!-- parte sinistra con logo -->
+            <div class="col-sx">
+              <div class="image-logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="Logo DC Comics" />
+              </div>
+            </div>
+            <!-- parte destra con navigazione -->
+            <div class="col-dx">
+              <nav>
+                <ul>
+                  <li>
+                    <a href="#">characters</a>
+                  </li>
+                  <li>
+                    <a class="current" href="#">comics</a>
+                  </li>
+                  <li>
+                    <a href="#">movies</a>
+                  </li>
+                  <li>
+                    <a href="#">tv</a>
+                  </li>
+                  <li>
+                    <a href="#">games</a>
+                  </li>
+                  <li>
+                    <a href="#">collectibles</a>
+                  </li>
+                  <li>
+                    <a href="#">videos</a>
+                  </li>
+                  <li>
+                    <a href="#">fans</a>
+                  </li>
+                  <li>
+                    <a href="#">news</a>
+                  </li>
+                  <li>
+                    <a href="#">shop</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </section>
+        </div>
+      </div>
+</header>
